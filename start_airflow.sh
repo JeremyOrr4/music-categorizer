eea18f21c8d4bd45eb99a81d559f67786d004281
Jeremy <jerorr4@yahoo.ca>
2025-07-04 13:49:57 -0400

    added RBAC role for airflow kube

--- start_airflow.sh
+++ start_airflow.sh
@@ -13,13 +13,23 @@
 # # Info
 # kubectl get all | grep airflow || echo "[INFO] All Airflow resources removed."
 
+helm uninstall airflow -n airflow
+# kubectl delete namespace airflow
+
+kubectl apply -f init_deploy/airflow_role/airflow-rbac.yaml
+kubectl get sa airflow-worker -n default                   
+kubectl get rolebinding airflow-pod-operator-binding -n default
+
+
 # start
 kubectl create namespace airflow
 helm repo add apache-airflow https://airflow.apache.org
 helm repo update
-helm install airflow apache-airflow/airflow --namespace airflow --debug --timeout 10m01s
-helm ls -n airflow 
 
 kubectl apply -f init_deploy/secret.yaml --namespace airflow
 helm upgrade --install airflow apache-airflow/airflow -n airflow -f init_deploy/values.yaml --debug --timeout 10m02s
-# kubectl port-forward svc/airflow-api-server 8080:8080
+helm ls -n airflow 
+
+
+# 2.9
+# kubectl port-forward svc/airflow-webserver 8080:8080 --namespace airflow
