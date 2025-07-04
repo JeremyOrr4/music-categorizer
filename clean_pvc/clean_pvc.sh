34aac4094eba195e823b9f61579c957a28a638f9
Jeremy <jerorr4@yahoo.ca>
2025-07-04 18:12:57 -0400

    added messages to clean pvc

--- clean_pvc/clean_pvc.sh
+++ clean_pvc/clean_pvc.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -euo pipefail
 
+echo "Starting cleanup of directories in /music-categorizer-data"
+
 cd /music-categorizer-data
 
 rm -rf lr-generator pcm_encoder pr-generator
+
+echo "Cleanup complete: deleted lr-generator, pcm_encoder, pr-generator"
