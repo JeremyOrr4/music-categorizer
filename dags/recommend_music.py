42f0645a5ee8b6c30786b5707f9c6ed9aa37686f
Jeremy <jerorr4@yahoo.ca>
2025-07-04 17:48:22 -0400

    fix to arg usage in recommendor dag

--- dags/recommend_music.py
+++ dags/recommend_music.py
@@ -38,7 +38,7 @@ with DAG(
         image="music-recommender:latest",
         image_pull_policy="IfNotPresent",
         cmds=["python", "main.py"],
-        arguments=["--recommend SONG_HERE"],
+        arguments=["--recommend", "SONG_HERE"],
         volumes=[volume],
         volume_mounts=[volume_mount],
         is_delete_operator_pod=True,
