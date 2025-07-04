cbfdeb356ff6f8348b19995a38e3f3992bcd1580
Jeremy <jerorr4@yahoo.ca>
2025-07-03 23:30:56 -0400

    updated for git sync dag method

--- start_airflow.sh
+++ start_airflow.sh
@@ -1,7 +1,25 @@
-helm uninstall airflow
-kubectl delete svc airflow-api-server --ignore-not-found
-kubectl delete svc airflow-webserver --ignore-not-found
+#!/bin/bash
+
+# echo "[INFO] Uninstalling Airflow Helm release..."
+# helm uninstall airflow
+# helm repo remove apache-airflow
+
+# echo "[INFO] Deleting Airflow services, pods, PVCs, configmaps, and secrets..."
+# kubectl delete all --all -n airflow
+# kubectl delete pvc --all -n airflow
+# kubectl delete configmap --all -n airflow
+# kubectl delete secret --all -n airflow
+
+# # Info
+# kubectl get all | grep airflow || echo "[INFO] All Airflow resources removed."
+
+# start
+kubectl create namespace airflow
 helm repo add apache-airflow https://airflow.apache.org
 helm repo update
-helm install airflow apache-airflow/airflow -f init_deploy/minimal-airflow-values.yaml
-kubectl port-forward svc/airflow-api-server 8080:8080
+helm install airflow apache-airflow/airflow --namespace airflow --debug --timeout 10m01s
+helm ls -n airflow 
+
+kubectl apply -f init_deploy/secret.yaml --namespace airflow
+helm upgrade --install airflow apache-airflow/airflow -n airflow -f init_deploy/values.yaml --debug --timeout 10m02s
+# kubectl port-forward svc/airflow-api-server 8080:8080
