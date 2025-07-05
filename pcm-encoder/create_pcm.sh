c6e6d539a7300ec770c167f8e6b6bc4deec97ea9
Jeremy <jerorr4@yahoo.ca>
2025-07-05 10:20:14 -0400

    Added better pcm ffmpeg formatting (44100)

--- pcm-encoder/create_pcm.sh
+++ pcm-encoder/create_pcm.sh
@@ -10,7 +10,7 @@ for file in /app/audio/*; do
     [ -f "$file" ] || continue
     base_name=$(basename "$file")
     out_file="$pcm_directory/${base_name%.*}.raw"
-    ffmpeg -i "$file" -f s16le -acodec pcm_s16le "$out_file"
+    ffmpeg -i "$file" -f s16le -acodec pcm_s16le -ar 44100 -ac 1 "$out_file"
 done
 
 
