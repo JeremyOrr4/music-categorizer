7771e02e2df979985f72e577a319c82c10e4f6dc
Jeremy <jerorr4@yahoo.ca>
2025-07-06 12:32:04 -0400

    added sanatization for audio in pcm

--- pcm-encoder/create_pcm.sh
+++ pcm-encoder/create_pcm.sh
@@ -11,7 +11,12 @@ get_audio() {
     for file in "$dir_name"/*; do
         [ -f "$file" ] || continue
         base_name=$(basename "$file")
-        out_file="$pcm_directory/${base_name%.*}.raw"
+
+        clean_name=$(echo "$base_name" | tr ' ' '_' | tr -cd '[:alnum:]_.-')
+        clean_name="${clean_name%.*}"
+
+        out_file="$pcm_directory/${clean_name}.raw"
+
         ffmpeg -i "$file" -f s16le -acodec pcm_s16le -ar 44100 -ac 1 "$out_file"
     done
 }
