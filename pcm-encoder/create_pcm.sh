e2c70828cfecaa5769ba3b1c486c43103a0d7f5e
Jeremy <jerorr4@yahoo.ca>
2025-07-06 12:17:16 -0400

    Added new pcm module for getting audio from pvc

--- pcm-encoder/create_pcm.sh
+++ pcm-encoder/create_pcm.sh
@@ -6,12 +6,17 @@ echo "Starting PCM-Encoder pod"
 pcm_directory="/music-categorizer-data/pcm_encoder"
 mkdir -p "$pcm_directory"
 
-for file in /app/audio/*; do
-    [ -f "$file" ] || continue
-    base_name=$(basename "$file")
-    out_file="$pcm_directory/${base_name%.*}.raw"
-    ffmpeg -i "$file" -f s16le -acodec pcm_s16le -ar 44100 -ac 1 "$out_file"
-done
+get_audio() {
+    dir_name="$1"
+    for file in "$dir_name"/*; do
+        [ -f "$file" ] || continue
+        base_name=$(basename "$file")
+        out_file="$pcm_directory/${base_name%.*}.raw"
+        ffmpeg -i "$file" -f s16le -acodec pcm_s16le -ar 44100 -ac 1 "$out_file"
+    done
+}
 
+get_audio "/app/audio"
+get_audio "/music-categorizer-data/audio"
 
 echo "Finished PCM-Encoder pod"
