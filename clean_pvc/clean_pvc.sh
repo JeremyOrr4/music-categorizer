3f7b9777d1691e4ebcdf7b798f9212346fab001d
Jeremy <jerorr4@yahoo.ca>
2025-07-06 13:01:25 -0400

    Added audio dir deletion for space

--- clean_pvc/clean_pvc.sh
+++ clean_pvc/clean_pvc.sh
@@ -5,6 +5,6 @@ echo "Starting cleanup of directories in /music-categorizer-data"
 
 cd /music-categorizer-data
 
-rm -rf lr-generator pcm_encoder pr-generator
+rm -rf lr-generator pcm_encoder pr-generator audio
 
 echo "Cleanup complete: deleted lr-generator, pcm_encoder, pr-generator"
